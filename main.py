686e211786103b17ee0d546ea3cae2647e91bc5a
Hanrocky <hanrocky.halim@gmail.com>
2023-01-09 18:25:23 +0700

    Added a simple map

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame as pg
 import sys
 from settings import *
+from map import *
 
 class Game:
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.new_game()
 
     def new_game(self):
-        pass
+        self.map = Map(self)
 
     def update(self):
         pg.display.flip()
@@ -18,8 +20,20 @@ class Game:
 
     def draw(self):
         self.screen.fill('black')
+        self.map.draw()
+
+    def check_event(self):
+        for event in  pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE): 
+                pg.quit()
+                sys.exit()
 
     def run(self):
         while True:
+            self.check_event()
             self.update()
             self.draw()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
