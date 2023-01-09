cdd786efed717d2a8b78e372ace2729dba288690
Hanrocky <hanrocky.halim@gmail.com>
2023-01-09 17:53:20 +0700

    First Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import sys
+from settings import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def new_game(self):
+        pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def run(self):
+        while True:
+            self.update()
+            self.draw()
---
+++ settings.py
@@ -0,0 +1,3 @@
+# Game settings
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
