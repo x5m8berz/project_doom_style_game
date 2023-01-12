30b00c586566d834781c7783cb78aa0d9e2c96a3
Hanrocky <hanrocky.halim@gmail.com>
2023-01-13 00:20:50 +0700

    Added animated sprite

--- sprite_object.py
+++ sprite_object.py
@@ -3,27 +3,32 @@ from settings import *
 
 
 class SpriteObject:
-    def __init__(self, game, path='./resources/sprites/static_sprites/candlebra.png', pos=(10.5, 3.5)):
+    def __init__(self, game, path='./resources/sprites/static_sprites/candlebra.png', pos=(12.5, 11.5), scale=0.5, shift=0.27):
         self.game = game
         self.player = game.player
         self.x, self.y = pos
         self.image = pg.image.load(path).convert_alpha()
         self.IMAGE_WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
-        self.IMAGE_RATIO =  self.IMAGE_WIDTH / self.image.get_height()
+        self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height()
         self.dx, self.dy, self.theta, self.screen_x, self.dist, self.norm_dist = 0, 0, 0, 0, 1, 1
         self.sprite_half_width = 0
+        self.SPRITE_SCALE = scale
+        self.SPRITE_HEIGHT_SHIFT = shift
 
     def get_sprite_projection(self):
-        proj = SCREEN_DIST / self.norm_dist
+        proj = SCREEN_DIST / self.norm_dist * self.SPRITE_SCALE
         proj_width, proj_height = proj * self.IMAGE_RATIO, proj
 
         image = pg.transform.scale(self.image, (proj_width, proj_height))
 
         self.sprite_half_width = proj_width // 2
-        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - proj_height // 2
+        height_shift = proj_height * self.SPRITE_HEIGHT_SHIFT
+        pos = self.screen_x - self.sprite_half_width, HALF_HEIGHT - \
+            proj_height // 2 + height_shift
 
-        self.game.raycasting.objects_to_render.append((self.norm_dist, image, pos))
+        self.game.raycasting.objects_to_render.append(
+            (self.norm_dist, image, pos))
 
     def get_sprite(self):
         dx = self.x - self.player.x
@@ -45,3 +50,10 @@ class SpriteObject:
 
     def update(self):
         self.get_sprite()
+
+
+class AnimatedSprite(SpriteObject):
+    def __init__(self, game, path='resources/sprites/animated_sprites/green_light/0.png', 
+                pos=(12.5, 11.5), scale=0.8, shift=0.15, animation_time=120):
+        super().__init__(game, path, pos, scale, shift)
+        self.animation_time = animation_time
