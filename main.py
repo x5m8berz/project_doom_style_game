015091625513b475ff126724087dd3e6aecdc938
Hanrocky <hanrocky.halim@gmail.com>
2023-01-12 07:28:16 +0700

    Added sprite projection

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
 
 
 class Game:
@@ -20,11 +21,13 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.static_sprite = SpriteObject(self)
 
     def update(self):
         self.delta_time = self.clock.tick(FPS)
         self.player.update()
         self.raycasting.update()
+        self.static_sprite.update()
         pg.display.flip()
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
