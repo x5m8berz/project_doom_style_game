72eef8973782c8f8d48aa521b6129c06efd2cad8
Hanrocky <hanrocky.halim@gmail.com>
2023-01-22 13:31:25 +0700

    Added npc movement

--- npc.py
+++ npc.py
@@ -21,12 +21,30 @@ class NPC(AnimatedSprite):
         self.pain = False
         self.ray_cast_value = False
         self.frame_counter = 0
+        self.player_search_trigger = False 
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
         # self.draw_ray_cast()
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx * self.size), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * self.size)):
+            self.y += dy
+
+    def movement(self):
+        next_pos = self.game.player.map_pos
+        next_x, next_y = next_pos
+        angle = math.atan2(next_y + 0.5 - self.y, next_x + 0.5 - self.x)
+        dx = math.cos(angle) * self.speed
+        dy = math.sin(angle) * self.speed
+        self.check_wall_collision(dx, dy)
 
     def animate_death(self):
         if not self.alive:
@@ -60,6 +78,13 @@ class NPC(AnimatedSprite):
             self.check_hit_in_npc()
             if self.pain:
                 self.animate_pain()
+            elif self.ray_cast_value:
+                self.player_search_trigger = True
+                self.animate(self.walk_images)
+                self.movement()
+            elif self.player_search_trigger:
+                self.animate(self.walk_images)
+                self.movement()
             else:
                 self.animate(self.idle_images)
         else:
