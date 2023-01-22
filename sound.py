51276285c427a3f8ac44e3291ee31087249ecc53
Hanrocky <hanrocky.halim@gmail.com>
2023-01-22 22:19:26 +0700

    Added more npc

--- sound.py
+++ sound.py
@@ -8,7 +8,9 @@ class Sound:
         self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
         self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
         self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
+        self.npc_shot.set_volume(0.2)
         self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
         self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
         self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
+        pg.mixer.music.set_volume(0.4)
         
