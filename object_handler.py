a98c546be9c020d53fdd656a6fafa8a8c0cb3314
Hanrocky <hanrocky.halim@gmail.com>
2023-01-23 19:35:03 +0700

    Change Map

--- object_handler.py
+++ object_handler.py
@@ -17,7 +17,7 @@ class ObjectHandler:
         self.npc_positions = {}
 
           # spawn npc
-        self.enemies = 5  # npc count
+        self.enemies = 10  # npc count
         self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
         self.weights = [70, 20, 10]
         self.restricted_area = {(i, j) for i in range(10) for j in range(10)}
